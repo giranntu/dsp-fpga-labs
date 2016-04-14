3fd0b042abf2e6b9c67fbff17d21f74ace295285
jisooj <jisooj@uw.edu>
2016-04-13 20:25:19 -0700

    q3 matlab added

---
+++ Labs/Lab_2/code/hw23.m
@@ -0,0 +1,28 @@
+% HW1 Question 5
+% Jake and Jisoo
+
+close all; clc; clear all;
+
+%% Read UART
+s = serial('COM5', 'BaudRate',115200); % Open the serial port to receive the data
+set(s,'InputBufferSize',20000); % set the size of input buffer
+fopen(s); % get ready to receive the data
+buffersize = 128*6; % set the size of instant read of buffer
+x = fread(s,buffersize,'int16'); % read the buffer when data arrive
+%%
+close all;
+flipped = x(1:128*3);
+original = x(128*3+1:end);
+% figure;
+% plot(flipped);
+% figure
+% plot(original);
+% 
+% flipped_frame1 = flipud(flipped(1:128));
+% flipped_frame2 = flipud(flipped(128+1:2*128));
+% flipped_frame3 = flipud(flipped(2*128+1:end));
+% 
+% original_from_flipped = [flipped_frame1; flipped_frame2; flipped_frame3];
+% figure;
+% plot(original_from_flipped);
+% original - original_from_flipped
